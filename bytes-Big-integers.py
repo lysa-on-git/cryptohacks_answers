426d972ebd6c25a388a7051ec3e9fb593980638c
lysa-on-git <maholy.ramaromanana@gmail.com>
2026-03-30 18:24:45 +0300

    bytes&BigIntegers

--- bytes-Big-integers.py
+++ bytes-Big-integers.py
@@ -1,3 +1,6 @@
 # / Python's PyCryptodome library implements this with the methods bytes_to_long() and long_to_bytes(). You will first have to install PyCryptodome and import it with from Crypto.Util.number import *
 
 import * 
+import Crypto.Util.number
+
+
